d8e11d5185d46293cd07b8d4266baa1168d51337
mleinad <101401882+mleinad@users.noreply.github.com>
2025-05-08 16:15:41 +0200

    base room prototype

--- Assets/Scripts/Memory/MemoryManager.cs
+++ Assets/Scripts/Memory/MemoryManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using AYellowpaper;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MemoryManager : MonoBehaviour
 {
+    [RequireInterface(typeof(IMemoryModifier))]
+    public List<UnityEngine.Object> memoryModifiers;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,14 @@ public class MemoryManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    void GetMemoryValue()
+    {
+        foreach (var m in memoryModifiers)
+        {
+          
+        }
     }
 }
